b9a2c92c8424fd399962a55129d0ca7ee53e3415
ian <yihengz@outlook.com>
2024-12-20 00:16:21 +0800

    docs: ✏️ readme

--- params.ts
+++ params.ts
@@ -1,4 +1,12 @@
-const dnsServers = ['8.8.8.8', '8.8.4.4', '1.1.1.1'];
+const dnsServers = [
+  '8.8.8.8', // Google公共DNS
+  '1.1.1.1', // Cloudflare公共DNS
+  '101.101.101.101', // 台湾Quad 101 DNS
+  '185.222.222.222', // DNS.SB公共DNS
+  '94.140.14.14', // AdGuard 公共DNS
+  '208.67.222.222', // OpenDNS(Cisco)
+  '9.9.9.9', // IBM Quad9
+];
 const domains = ['github.com', 'objects.githubusercontent.com'];
 
 type HostObj = {
