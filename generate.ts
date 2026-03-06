e84fd8f42978862ca539154b6fa843caa9167042
ian <yihengz@outlook.com>
2026-03-06 18:19:50 +0800

    update

--- generate.ts
+++ generate.ts
@@ -2,4 +2,6 @@ const response = await fetch(
   'https://gitee.com/api/v5/gists/hzp8a5rsjygdeuon0k3qt75/comments/?access_token=c88b3a2aaaf8f067d61780676cf5376f',
 );
 const res = await response.json();
-await Bun.write('hosts', res[0].body);
+const hosts = res[0].body;
+await Bun.write('hosts', hosts);
+await Bun.write('README.md', `***text\n${hosts}***`);
