476b75637afb149691d4f3f3e72ed1c56d2e0575
ian <yihengz@outlook.com>
2026-03-06 14:02:51 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 TODO:
 
 - [x] gitee 同步
-- [ ] gitee go 每天检查 ip 可用性，若不可用则生成失效域名列表
-- [ ] 失效列表同步到 GitHub
-- [ ] GitHub action 查询新 ip 并生成 json 文件，同步到 gitee
+- [x] GitHub action 查询新 ip 并生成 json 文件，同步到 gitee
 - [ ] gitee go 检查新 ip 是否可用，并更新 hosts 文件
--- generate.ts
+++ generate.ts
@@ -1,24 +1,35 @@
-import { ping } from "./src";
+import { ping } from './src';
+
 type Hosts = Record<string, string>;
 
 async function filterIPsByLatency(hosts: Hosts) {
-  const pingPromises = Object.keys(hosts).map((ip) =>
-    ping(ip).then((latency) => ({ ip, latency })),
-  );
+  const pingPromises = Object.keys(hosts).map((ip) => ping(ip).then((latency) => ({ ip, latency })));
   const results = await Promise.allSettled(pingPromises);
   for (const result of results) {
-    if (result.status === "fulfilled" && result.value.latency > 400) {
+    if (result.status === 'fulfilled' && result.value.latency > 400) {
       delete hosts[result.value.ip];
     }
   }
 }
 
-const hostsJSON = await Bun.file("./candidates.json").json();
+const hostsJSON = await Bun.file('./candidates.json').json();
 await filterIPsByLatency(hostsJSON);
 
 const hostsFileContent = Object.entries(hostsJSON)
   .map(([ip, host]) => `${ip} ${host}`)
-  .join("\n");
+  .join('\n');
 
-await Bun.write("hosts", hostsFileContent);
-console.log("Hosts file generated");
+const response = await fetch('https://gitee.com/api/v5/gists/hzp8a5rsjygdeuon0k3qt75/comments/49337638', {
+  method: 'PATCH',
+  headers: {
+    'Content-Type': 'application/json',
+    charset: 'UTF-8',
+  },
+  body: JSON.stringify({ access_token: 'c88b3a2aaaf8f067d61780676cf5376f', body: hostsFileContent }),
+});
+
+const result = await response.json();
+console.log(result);
+
+// await Bun.write('hosts', hostsFileContent);
+console.log('Hosts file generated');
